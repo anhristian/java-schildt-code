28157b72213d67fafd115f93c50e22f8be7dfdbe
Anastasia Hristian <an.hristian@gmail.com>
2020-08-23 00:11:18 -0600

    Displayed table with true && false.

--- src/edu/cnm/deepdive/DisplayThruthTable.java
+++ src/edu/cnm/deepdive/DisplayThruthTable.java
@@ -5,11 +5,31 @@ public class DisplayThruthTable {
   public static void main(String[] args) {
 
     boolean p, q;
-    System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+    System.out.println("P \tQ \tAND \tOR \tXOR \tNOT");
 
     p = true;
     q = true;
+    System.out.print(p + "\t" + q + "\t");
+    System.out.print((p&q) + "\t" + (p|q) + "\t");
+    System.out.println((p^q) + "\t" + (!p));
 
+    p = true;
+    q = false;
+    System.out.print(p + "\t" + q + "\t");
+    System.out.print((p&q) + "\t" + (p|q) + "\t");
+    System.out.println((p^q) + "\t" + (!p));
+
+    p = false;
+    q = true;
+    System.out.print(p + "\t" + q + "\t");
+    System.out.print((p&q) + "\t" + (p|q) + "\t");
+    System.out.println((p^q) + "\t" + (!p));
+
+    p = false;
+    q = false;
+    System.out.print(p + "\t" + q + "\t");
+    System.out.print((p&q) + "\t" + (p|q) + "\t");
+    System.out.println((p^q) + "\t" + (!p));
   }
 
 }
