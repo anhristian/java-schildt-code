04d69e576f57d53d441527b0d3bf2ebb3bcc7587
Anastasia Hristian <an.hristian@gmail.com>
2020-08-23 00:37:20 -0600

    Substituted true && false with 1 && 0.

--- src/edu/cnm/deepdive/DisplayThruthTable.java
+++ src/edu/cnm/deepdive/DisplayThruthTable.java
@@ -9,27 +9,28 @@ public class DisplayThruthTable {
 
     p = true;
     q = true;
-    System.out.print(p + "\t" + q + "\t");
-    System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + (!p));
+
+    System.out.print((p ? 1 : 0) + "\t" + (q ? 1 : 0) + "\t");
+    System.out.print(((p&q)? 1 : 0) + "\t" + ((p|q)? 1 : 0) + "\t");
+    System.out.println(((p^q)? 1 : 0) + "\t" + ((!p)? 1 : 0));
 
     p = true;
     q = false;
-    System.out.print(p + "\t" + q + "\t");
-    System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + (!p));
+    System.out.print((p ? 1 : 0) + "\t" + (q ? 1 : 0) + "\t");
+    System.out.print(((p&q) ? 1 : 0) + "\t" + ((p|q) ? 1 : 0) + "\t");
+    System.out.println(((p^q) ? 1 : 0) + "\t" + ((!p) ? 1 : 0));
 
     p = false;
     q = true;
-    System.out.print(p + "\t" + q + "\t");
-    System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + (!p));
+    System.out.print((p? 1 : 0) + "\t" + (q? 1 : 0) + "\t");
+    System.out.print(((p&q)? 1 : 0) + "\t" + ((p|q)? 1 : 0) + "\t");
+    System.out.println(((p^q)? 1 : 0) + "\t" + ((!p)? 1 : 0));
 
     p = false;
     q = false;
-    System.out.print(p + "\t" + q + "\t");
-    System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + (!p));
+    System.out.print((p? 1 : 0) + "\t" + (q? 1 : 0) + "\t");
+    System.out.print(((p&q)? 1 : 0) + "\t" + ((p|q)? 1 : 0) + "\t");
+    System.out.println(((p^q)? 1 : 0) + "\t" + ((!p)? 1 : 0));
   }
 
 }
