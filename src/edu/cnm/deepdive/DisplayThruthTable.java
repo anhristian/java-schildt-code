42be576fae2531995613081b71e4d108eea2e0bc
Anastasia Hristian <an.hristian@gmail.com>
2020-08-22 23:59:49 -0600

    In progress.

--- src/edu/cnm/deepdive/DisplayThruthTable.java
+++ src/edu/cnm/deepdive/DisplayThruthTable.java
@@ -5,6 +5,11 @@ public class DisplayThruthTable {
   public static void main(String[] args) {
 
     boolean p, q;
+    System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+
+    p = true;
+    q = true;
+
   }
 
 }
